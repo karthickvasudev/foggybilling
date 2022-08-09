de72e8c09ebe65d6f1759909fee321eac125fca1
karthick vasudevan <karthick.tungsten@gmail.com>
2022-08-09 19:22:18 +0530

    completed

--- src/app/billing-page/billing-page.component.ts
+++ src/app/billing-page/billing-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { GetTransactionBill } from 'src/modals/getTransactionBill';
 
 @Component({
   selector: 'app-billing-page',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./billing-page.component.css']
 })
 export class BillingPageComponent implements OnInit {
+  baseUrl = "http://192.168.0.102:8080/api/v1/bill"
+  billUrl: any;
 
-  constructor() { }
+  public response!: GetTransactionBill;
+
+  @ViewChild('billSection', { static: false })
+  private billSection !: ElementRef
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute, private route: Router) { }
 
   ngOnInit(): void {
+    this.billUrl = this.activatedRoute.snapshot.paramMap.get("billUrl")
+    this.billPageLoad()
   }
 
+  billPageLoad() {
+    this.http.get<GetTransactionBill>(this.baseUrl + "/" + this.billUrl)
+      .subscribe({
+        next: (response) => {
+          this.response = <GetTransactionBill>response;
+        }, error: (err) => {
+          this.route.navigate(["/billnotfound"])
+        }
+      })
+  }
+
+  public getProductRate(price: any, count: any) {
+    return price / count
+  }
+
+  getFormattedDate(date: any) {
+    return date.replace(" ", "<br>")
+  }
+
+  downloadThisBill() {
+
+    html2canvas(this.billSection.nativeElement).then(canvas => {
+      var imgHeight = canvas.height * 208 / canvas.width;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+
+      let pdf = new jsPDF('p', 'px', [canvas.width, canvas.height],true);
+
+      
+
+      pdf.addImage(contentDataURL, 'JPEG', 20, 0, pdf.internal.pageSize.width-40, pdf.internal.pageSize.height)
+
+      let customerId = this.response.customerDetails.id
+      let orderId = this.response.orderDetails.id
+      let deliveryDate = this.response.orderDetails.deliveredDate;
+      pdf.save(orderId + "_" + customerId + "_" + deliveryDate)
+    })
+
+  }
 }
